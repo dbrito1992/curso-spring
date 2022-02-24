effefb2cd945ab39ca39ab765622d3db3ca82987
Diego Brito <1992diego1992@gmail.com>
2022-02-23 22:44:54 -0300

    Classes Services

--- src/main/java/com/curso/resourcies/UserResource.java
+++ src/main/java/com/curso/resourcies/UserResource.java
@@ -1,19 +1,33 @@
 package com.curso.resourcies;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.curso.domain.User;
+import com.curso.services.UserService;
 
 @RestController
 @RequestMapping(value="/user")
 public class UserResource {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User us = new User(1, "Diego Brito", "diego@teste.com.br", 29);
-		return ResponseEntity.ok().body(us);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> obj = userService.findAll();
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Integer id){
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/curso/services/UserService.java
@@ -0,0 +1,27 @@
+package com.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.curso.domain.User;
+import com.curso.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Integer id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get();
+	}
+
+}
