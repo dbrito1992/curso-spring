28020b640bf363de17a75e901952ab179e4e1aa7
Diego Brito <1992diego1992@gmail.com>
2022-03-19 02:03:02 -0300

    Exceções Delete

--- src/main/java/com/curso/domain/Order.java
+++ src/main/java/com/curso/domain/Order.java
@@ -18,6 +18,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.curso.enums.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Table(name = "tb_order")
@@ -27,6 +28,8 @@ public class Order implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
 	@ManyToOne
